e27d0e2f3010fb535149792abd29b2fe1a26781f
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-29 10:48:46 +0200

    Week2 excersise on array

---
+++ src/main/java/se/lexicon/rajashree/Diagonal2DArray.java
@@ -0,0 +1,22 @@
+package se.lexicon.rajashree;
+
+public class Diagonal2DArray {
+
+    public static void main(String[] args) {
+
+
+        int num = 1;
+        int[][] a = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+
+                a[i][j] = num+1;
+
+                System.out.print(a[i][j]);
+
+            }
+
+
+
+        }}}
---
+++ src/main/java/se/lexicon/rajashree/TwoDimensionalArray.java
@@ -0,0 +1,23 @@
+package se.lexicon.rajashree;
+public class TwoDimensionalArray {
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
